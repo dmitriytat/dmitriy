fb1febfffce022e88d71b80c6a5db42c4e042bb5
Dmitriy <tatarintsevd@gmail.com>
2021-06-27 16:44:26 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 addEventListener("fetch", (event) => {
   event.respondWith(
-    new Response("Hello world", {
+    new Response("Hello! I am Dmitriy from https://dmitriy.tatar", {
       status: 200,
       headers: { "content-type": "text/plain" },
     }),
